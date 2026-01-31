7258c6aa512719d0e8aacb05d027ab0fe3524345
Amal Chulliyat Jose <amaljose1998@gmail.com>
2026-01-31 16:37:48 +0100

    exp8 config

--- hdv/hdv_dbn/config.py
+++ hdv/hdv_dbn/config.py
@@ -106,9 +106,9 @@ class TrainingConfig:
     poe_em_lr: float = 3e-3
     poe_em_steps: int = 20
 
-    learn_pi0: bool = False  
+    learn_pi0: bool = True
     pi0_alpha: float = 0.0
-    disable_discrete_obs: bool = True 
+    disable_discrete_obs: bool = False
     bern_weight: float = 1     #(dont change)                
     lc_weight: float = 25   
     # Lane-change imbalance handling in EM
@@ -147,7 +147,7 @@ class TrainingConfig:
 
     use_wandb: bool = True
     wandb_project: str = "hdv_dbn_highd"
-    wandb_run_name: Optional[str] = "1.poe-sticky_cpd-uni_pi-lc_none-bern_off"
+    wandb_run_name: Optional[str] = "8.poe-sticky_cpd-lern_pi-lc_none-bern_on"
 
     backend: Literal["torch"] = "torch"
     device: Literal["cuda", "cpu"] = "cuda"
