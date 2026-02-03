2fd20435ce874cf630af0efe37c0dd404d0e7471
Amal Chulliyat Jose <amaljose1998@gmail.com>
2026-02-03 14:30:14 +0100

    update

--- hdv/hdv_dbn/utils/eval_core.py
+++ hdv/hdv_dbn/utils/eval_core.py
@@ -1,10 +1,10 @@
 # eval_core.py (thin facade)
 from .eval_common import scale_obs_masked, seq_key
-from .eval_predictive import evaluate_online_predictive_ll, evaluate_anticipatory_predictive_ll, evaluate_frozen_belief_online_ll, evaluate_iid_baseline
+from .eval_predictive import evaluate_online_predictive_ll, evaluate_anticipatory_predictive_ll
 from .eval_diagnostics import evaluate_checkpoint
 
 __all__ = [
     "scale_obs_masked", "seq_key",
     "evaluate_online_predictive_ll", "evaluate_anticipatory_predictive_ll",
-    "evaluate_checkpoint", "evaluate_frozen_belief_online_ll", "evaluate_iid_baseline"
+    "evaluate_checkpoint"
 ]
